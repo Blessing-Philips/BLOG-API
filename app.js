d14ed363d81ec5d61942919c96195dce8ffda3a0
Blessing Philips <blessingphilips57@gmail.com>
2024-07-14 10:22:57 +0100

    Second comit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const bcryptjs = require('bcryptjs');
 
 const db = require('./database/database');
+const home = require('./routes/home.route');
+const Signup = require('./routes/user.route');
 
 const app = express();
 
@@ -10,11 +13,8 @@ app.use(express.json())
 app.use(cors())
 dotenv.config()
 
-app.get('/', (req, res) => {
-    res.json({"response": "api working"})
-})
-
-
+app.use(home);
+app.use('/api/', Signup);
 
 const port = process.env.PORT
 app.listen(port, () => {
