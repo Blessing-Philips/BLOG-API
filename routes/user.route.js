c32eb6e48c24c01a3e8b74ac2184cf9692b79dbf
Blessing Philips <blessingphilips57@gmail.com>
2024-07-18 09:30:29 +0100

    Addition of google auth route

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,10 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const { signup,
-    signin
+    signin,
+    googleAuth
 } = require('../controllers/user.controller')
 
 router.post('/signup', signup);
 router.post('/signin', signin);
+router.post('/google', googleAuth);
 
 module.exports = router;
